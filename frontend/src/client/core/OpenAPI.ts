8a2eb37fcea2aee85e0a1df1342c5cb12f42a73a
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2026-05-16 17:47:57 +0200

    00-11 · User Custom Ingredient UI Implementation (#77)
    Closes #11
    
    ---------
    
    Co-authored-by: Bendeguz Kovacs <mate-bendeguz.kovacs@flixbus.com>

--- frontend/src/client/core/OpenAPI.ts
+++ frontend/src/client/core/OpenAPI.ts
@@ -48,7 +48,7 @@ export const OpenAPI: OpenAPIConfig = {
   TOKEN: undefined,
   USERNAME: undefined,
   VERSION: "0.1.0",
-  WITH_CREDENTIALS: true,
+  WITH_CREDENTIALS: false,
   interceptors: {
     request: new Interceptors(),
     response: new Interceptors(),
